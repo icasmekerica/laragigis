b729a076e2e1a28e3426065bd3e2d3a8455e8a33
Ivan Radosavljevic <ir20180252@student.fon.bg.ac.rs>
2024-01-07 18:42:46 +0100

    promenjena home stranica + dodati modeli manuelno - ica

--- laragigs/routes/web.php
+++ laragigs/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,48 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' =>[ 
+        [
+            'id' => 1,
+            'title' => 'Listing One',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis nisi commodo, aliquet nisl in, venenatis turpis.'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Listing Two',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis nisi commodo, aliquet nisl in, venenatis turpis.'
+        ]
+       
+        ]
+    ]);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Route::get('/hello', function(){
+    return response('<h1>Hello World</h1>');
+});
+
+Route::get('/posts/{id}', function($id){
+    //ddd($id);
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function(Request $request){
+return($request->name . ' ' . $request->city);
+    
+});*/
