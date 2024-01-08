2866c3473c60bfa1ed06a398e6c90f3dae54781a
Ivan Radosavljevic <ir20180252@student.fon.bg.ac.rs>
2024-01-08 16:57:44 +0100

    Dodate slike, napravljene karte, dotdat html

--- laragigs/routes/web.php
+++ laragigs/routes/web.php
@@ -24,11 +24,13 @@ Route::get('/', function () {
 });
 
 // Single Listing
-Route::get('/listings/{id}', function($id){
+Route::get('/listings/{listing}', function(Listing $listing){
     return view('listing', [
         'heading' => 'Listing $id',
-        'listing' => Listing::find($id)
+        'listing' =>  $listing
     ]);
+
+    
 });
 
 
