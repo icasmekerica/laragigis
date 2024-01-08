86c0787a7bcfae0dd930514fff8330bc5b612cd8
Ivan Radosavljevic <ir20180252@student.fon.bg.ac.rs>
2024-01-08 14:10:44 +0100

    Napravljen Factory

--- laragigs/app/Models/Listing.php
+++ laragigs/app/Models/Listing.php
@@ -2,37 +2,10 @@
 
 namespace App\Models;
 
-class Listing {
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function all(){
-        return [ 
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis nisi commodo, aliquet nisl in, venenatis turpis.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis nisi commodo, aliquet nisl in, venenatis turpis.'
-            ]
-           
-            ];
-        
-    }
-
-    public static function find($id){
-        $listings = self::all();
-
-        foreach($listings as $listing){
-            if($listing['id'] == $id){
-                return $listing;
-            }
-
-        }
-
-    }
-
+class Listing extends Model
+{
+    use HasFactory;
 }
-
-?>
